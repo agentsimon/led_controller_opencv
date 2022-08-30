dc096a18a556eb79b83800b391fec89d4d778c30
agentsimon <31639346+agentsimon@users.noreply.github.com>
2022-08-30 14:19:00 +0700

    Update led_opncv.py

--- led_opncv.py
+++ led_opncv.py
@@ -1,82 +1,71 @@
-#!/usr/bin/env python3
-import cv2
-import numpy as np
-#import imutils
-# Simple test for NeoPixels on Raspberry Pi
+import serial
 import time
-import board
-import neopixel
+import cv2
+
+import argparse  
+
+#The following line is for serial over GPIO
+port = '/dev/cu.usbmodem142301' # note I'm using Mac OS-X
 
 
-# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board>
-# NeoPixels must be connected to D10, D12, D18 or D21 to work.
-pixel_pin = board.D18
+ard = serial.Serial(port,9600,timeout=5)
+time.sleep(2) # wait for Arduino
 
-# The number of NeoPixels
-num_pixels = 100
+vid = cv2.VideoCapture(0)
 
-# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green>
-# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
-ORDER = neopixel.GRB
 
-pixels = neopixel.NeoPixel(
-    pixel_pin, num_pixels, brightness=0.8, auto_write=False, pixel_order=ORDER)
 
-# Reverse LEDs
-def reverse(seq, start, stop):
-    size = stop + start
-    for i in range(start, (size + 1) // 2 ):
-        j = size - i
-        seq[i], seq[j] = seq[j], seq[i]
-
-cap = cv2.VideoCapture(0)
-#trackbar callback fucntion does nothing but required for trackbar
-def nothing(x):
-	pass
-#create a seperate window named 'controls' for trackbar
-cv2.namedWindow('controls')
-
-#create trackbar in 'controls' window with name 'r''
-cv2.createTrackbar('control_lower','controls',15,255,nothing)
-cv2.createTrackbar('control_upper','controls',15,255,nothing)
+""" def write_read(x):
+    arduino.write(bytes(x, 'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return data """
 
 while(True):
-    # Capture frame-by-frame
+      
+    # Capture the video frame
+    # by frame
     
-    ret, frame = cap.read()
-   
-    #frame = imutils.resize(frame,80, 60)
-    frame =cv2.resize(frame,(10, 10), interpolation = cv2.INTER_LINEAR)
-    print(frame.shape)
-    # Define sliders on the slider image
-    threshold_lower= int(cv2.getTrackbarPos('control_lower','controls'))
-    threshold_upper= int(cv2.getTrackbarPos('control_upper','controls'))
-    # Our operations on the frame come here
+    ret, frame = vid.read()
+    frame =cv2.resize(frame,(8, 8), interpolation = cv2.INTER_LINEAR)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Thresholding using THRESH_BINARY_INV 
-    #th, dst = cv2.threshold(gray,threshold_lower, threshold_upper, cv2.THRESH_BINARY_INV); 
-    #Basic threshold example 
-    th,dst = cv2.threshold(gray, threshold_lower, threshold_upper, cv2.THRESH_BINARY);
-    print("Shape", dst.shape)
-    #pixel_color = frame.sum(axis=2)
-    # Print RGB values for the image
-    color = []
-    for x in range (0,10,1):
-        for y in range(0,10,1):
-            pixel_color = int(dst[y,x])
-            color.append(pixel_color)
-    print(color)
-    for color_led in range(len(color)):
-        if color[color_led]> 0:
-            pixels[color_led] = (0,0,0)
-        else:
-            pixels[color_led] =(255,255,255)
-    pixels.show()
-    cv2.imshow('frame',dst)
+    _,thres_image = cv2.threshold(gray, 120,255,0)
+    # Display the resulting frame
+    #print(thres_image)
+    cv2.imshow('frame', thres_image)
 
+    res =thres_image
+    for line in enumerate(thres_image):
+        for index2 in enumerate(thres_image[line[0]]):
+            if thres_image[line[0]][index2[0]] > 0:
+                res[line[0]][index2[0]] = 1
+
+    #print(res) 
+    #print(type(res))  
+    list1 = res.tolist()
+    #value = write_read(list1)
+    complete_list=[]
+    for i in list1:
+        for j in i:
+            complete_list.append(j)
+    print("List is ", complete_list)
+    list_numbers_string = "".join(str(string_list) for string_list in complete_list)
+    print("String is", list_numbers_string)
+    ard.flush()
+    print ("Python value sent: ", list_numbers_string)
+    ard.write(str.encode(list_numbers_string))
+    time.sleep(1)
+    # Serial read section
+    msg = ard.read(ard.inWaiting()) # read all characters in buffer
+    print ("Message from arduino: ")
+    print (msg)       
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-# When everything done, release the capture
-cap.release()
+  
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
 cv2.destroyAllWindows()
